975fe5caf61f9400703da35c812668c40d9937d0
Lape <larape1993@gmail.com>
2022-08-23 16:57:12 +0100

    Updated create cart-item handler

--- handlers/cart.js
+++ handlers/cart.js
@@ -1,11 +1,26 @@
 const User = require("../models/users");
 const Item = require("../models/cartItem");
 
-const cart = (req, res, next) => {
+const cart = async (req, res, next) => {
   if (req.body) {
     const item = req.body;
     item.owner = req.user.userId;
-    console.log({ item }, req.user);
+    const oldItem = await Item.findOne({ name: item.name });
+    if (oldItem) {
+      try {
+        const result = await oldItem.updateOne(
+          { quantity: item.quantity, price: item.price },
+          { $set: oldItem }
+        );
+        return res.status(200).send({
+          message: "Cart Updated",
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500);
+      }
+    }
+
     Item.create(item)
       .then((result) => {
         res.status(200).send({
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const User = require("../models/users");
 const auth = require("../auth");
 const postUser = require("../handlers/user");
 const login = require("../handlers/login");
+const cart = require("../handlers/cart");
 
 router.get("/users", (req, res, next) => {
   User.find({})
@@ -26,6 +27,6 @@ router.get("/auth-endpoint", auth, (req, res) => {
   res.json({ message: "You are authorized to access me" });
 });
 
-router.post("/cart-item", auth);
+router.post("/cart", auth, cart);
 
 module.exports = router;
